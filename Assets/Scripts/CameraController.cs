ea5044ba955af529f647937591a69c9fbaf9aad8
Hexaptera <hexaptera6@gmail.com>
2026-03-12 16:32:14 +0100

    Basic movement, player manager and dynamic camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        
+        if (players.Length == 0)
+        { 
+            return;
+        }
+
+        Vector3 boundaryStart = players[0].transform.position;
+        Vector3 boundaryEnd = players[0].transform.position;
+        foreach (GameObject player in players)
+        {
+            if (player.transform.position.x < boundaryStart.x)
+            {
+                boundaryStart.x = player.transform.position.x;
+            }
+            if (player.transform.position.y < boundaryStart.y)
+            {
+                boundaryStart.y = player.transform.position.y;
+            }
+            if (player.transform.position.z < boundaryStart.z)
+            {
+                boundaryStart.z = player.transform.position.z;
+            }
+            if (player.transform.position.x > boundaryEnd.x)
+            {
+                boundaryEnd.x = player.transform.position.x;
+            }
+            if (player.transform.position.y > boundaryEnd.y)
+            {
+                boundaryEnd.y = player.transform.position.y;
+            }
+            if (player.transform.position.z > boundaryEnd.z)
+            {
+                boundaryEnd.z = player.transform.position.z;
+            }
+        }
+        float distance = Mathf.Clamp(((boundaryStart - boundaryEnd).magnitude + 4) * 0.8f, 5, 20);
+        transform.position = (boundaryStart + boundaryEnd) / 2 - distance * transform.forward;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+    PlayerInput playerInput;
+    InputAction moveAction;
+    InputAction jumpAction;
+
+    [SerializeField] GameObject feet;
+    [SerializeField] LayerMask floorCollisionMask;
+
+    [SerializeField] float speed;
+    [SerializeField] float acceleration;
+    [SerializeField] float velocityDamping;
+    [SerializeField] float gravity;
+    [SerializeField] float jumpStrength;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = moveAction.ReadValue<Vector2>();
+        var vel = rigidBody.linearVelocity;
+
+        if (vel.magnitude < speed)
+        {
+            vel += ((transform.right * move.x + transform.forward * move.y) * acceleration * Time.deltaTime);
+        }
+
+        bool hit = Physics.Raycast(feet.transform.position, new Vector3(0, -1, 0), 0.1f, floorCollisionMask);
+        if (jumpAction.WasPressedThisFrame() && hit)
+        {
+            vel.y = jumpStrength;
+        }
+        else
+        {
+            vel.y -= gravity * Time.deltaTime;
+        }
+
+        vel.x *= Mathf.Pow(velocityDamping, Time.deltaTime);
+        vel.z *= Mathf.Pow(velocityDamping, Time.deltaTime);
+        rigidBody.linearVelocity = vel;
+    }
+}
